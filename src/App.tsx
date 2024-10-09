d771a1e314b6fe68001e36fc31b716bc61e7ff60
camposyuri <yurinapoleao@gmail.com>
2024-10-09 20:03:39 -0300

    fix: ci/cd lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { AppBar } from "./components/AppBar";
 import { TodosList } from "./components/TodosList";
-import { GlobalProvider } from './contexts/GlobalContext'
-import { useRenderCounter } from "./hooks/useRenderCounter"
+import { GlobalProvider } from "./contexts/GlobalContext";
+import { useRenderCounter } from "./hooks/useRenderCounter";
 
 export function App() {
   useRenderCounter("App");
