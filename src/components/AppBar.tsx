eb359f5ea7bf182172b164c9f7ad668cb8bbdbca
camposyuri <yurinapoleao@gmail.com>
2024-10-10 21:19:31 -0300

    fix: fix something

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -14,7 +14,7 @@ export function AppBar() {
           <img src={logo} alt="JStack" width="80" />
           <TodosCounter />
         </div>
-        <p>Hello world</p>
+        <p>Hello world </p>
         <UserMenu />
       </div>
     </header>
